9856c34fd94d8a7d46627475ac7b8984572801dc
unknown <naharwalneha@gmail.com>
2024-06-12 20:55:31 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,18 +1,22 @@
 package demo;
 
+import java.time.Duration;
+import java.util.logging.Level;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
-import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
@@ -21,6 +25,66 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException{
+        System.out.println("Start of testCase01");
+        //navigate to google form
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        //Enter name
+        WebElement name = driver.findElement(By.xpath("//input[@aria-labelledby='i1' and contains(@class, 'whsOnd zHQkBf')]"));
+        Thread.sleep(2000);
+        name.click();
+        name.sendKeys("Crio Learner");
+        //practicing automation
+        WebElement automation = driver.findElement(By.xpath("//textarea[@aria-labelledby='i5' and contains(@class, 'KHxj8b tL9Q4c')]"));
+        Thread.sleep(2000);
+        automation.click();
+        automation.sendKeys("I want to be the best QA Engineer! 1718128346");
+        //experience
+        driver.findElement(By.xpath("//div[contains(@id, 'i13')]")).click();
+        Thread.sleep(2000);
+        //skills java, selenium, testNG
+        driver.findElement(By.xpath("//div[contains(@id, 'i30')]")).click();
+        driver.findElement(By.xpath("//div[contains(@id, 'i33')]")).click();
+        driver.findElement(By.xpath("//div[contains(@id, 'i39')]")).click();
+        //addressed by
+        driver.findElement(By.xpath("//span[contains(text(), 'Choose')]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[2]/div[4]")).click();
+        Thread.sleep(1000);
+        //date
+        WebElement date = driver.findElement(By.xpath("//input[@aria-labelledby='i50']"));
+        // Perform Tab key action
+        Actions actions = new Actions(driver);
+        Thread.sleep(1000);
+        actions.sendKeys(Keys.TAB).perform();
+        // JavascriptExecutor js = (JavascriptExecutor) driver;
+        // js.executeScript("arguments[0].value = '2024-06-05';", date);
+        date.sendKeys("05-06-2024");
+        Thread.sleep(1000);
+        //time hour, minute
+        WebElement hour = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+        hour.click();
+        hour.sendKeys("07");
+        WebElement minute = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+        minute.click();
+        minute.sendKeys("30");
+        //click on submit
+        driver.findElement(By.xpath("//span[contains(text(), 'Submit')]")).click();
+        Thread.sleep(1000);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Thanks for your response, Automation Wizard!')]")));
+        String successMessage = message.getText();
+        if(successMessage.contains("Thanks for your response, Automation Wizard!")){
+            System.out.println("Thanks for your response, Automation Wizard!");
+        }
+
+        System.out.println("End of testCase01");
+
+    
+    }
 
      
     /*
